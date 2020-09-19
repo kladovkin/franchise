255bde614914614307bafdc4d8f8e3ca4d990137
Andrey Sidorov <takandar@gmail.com>
2020-09-19 17:11:00 +0300

    working on slide 1

--- src/components/slide_01.vue
+++ src/components/slide_01.vue
@@ -1,7 +1,14 @@
 <template>
   <article>
     <router-link to='/' class='logo' />
-    Технологичный бизнес на вашей территории
+    <div class="fc">
+      <div class="f-column">
+        <h1>Технологичный бизнес на вашей территории</h1>
+        <p>Станьте партнером и зарабатывайте на&nbsp;своей недвижимости</p>
+      </div>
+      <div class="f-column">
+      </div>
+    </div>
   </article>
 </template>
 
@@ -14,6 +21,34 @@ export default {
 <style scoped lang='sass'>
 article
   background: #f44f0c
+  color: #fff
+
+  +lte_ipad
+    text-align: center
+    padding-top: rem(110px)
+
+  +gte_laptop
+    padding-top: 200px
+    min-height: 600px
+
+h1
+  +lte_ipad
+    margin-bottom: 20px
+
+  +gte_laptop
+    max-width: 520px
+    margin-bottom: 24px
+
+p
+  letter-spacing: -0.01em
+
+  +lte_ipad
+    font-size: rem(14px)
+    line-height: rem(20px)
+
+  +gte_laptop
+    font-size: 24px
+    line-height: 28px
 
 .logo
   background-image: url(../assets/slide_01/logo.svg)
@@ -21,7 +56,20 @@ article
   background-size: contain
   position: absolute
 
+  +lte_ipad
+    height: rem(52px)
+    left: rem(16px)
+    top: rem(14px)
+    width: rem(198px)
+
+  +laptop
+    left: scale-between(31px, 47px, 1024px, 1280px)
+
   +gte_laptop
     height: 53px
+    top: 42px
     width: 200px
+
+  +gte_desktop
+    left: 47px
 </style>
