b1cbf040848126a306bcf9330612663ccd18d43c
Andrey Sidorov <takandar@gmail.com>
2020-09-19 15:58:47 +0300

    webfontloader lato

--- src/app.vue
+++ src/app.vue
@@ -4,11 +4,15 @@
 
 <script>
 import Landing from '@/views/landing.vue';
+import requireFonts from '@/utils/require_fonts';
 
 export default {
   name: 'App',
   components: {
     Landing
+  },
+  created() {
+    requireFonts('yjq4zkd');
   }
 };
 </script>
