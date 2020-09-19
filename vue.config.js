ac19add42e234a6a4a0ff84d9a7d01a4c573a298
Andrey Sidorov <takandar@gmail.com>
2020-09-19 16:29:44 +0300

    global styles

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?
     '/' :
-    '/'
+    '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "@/stylesheets/globals.sass";'
+      }
+    }
+  }
 };
