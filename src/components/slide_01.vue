982d3faf29d3dfe737b700692f2c3c2f6ce0d122
Andrey Sidorov <takandar@gmail.com>
2020-09-20 13:00:57 +0300

    working on backgrounds

--- src/components/slide_01.vue
+++ src/components/slide_01.vue
@@ -2,11 +2,11 @@
   <article>
     <router-link to='/' class='logo' />
     <div class="fc">
-      <div class="f-column">
+      <div class="f-left">
         <h1>Технологичный бизнес на вашей территории</h1>
         <p>Станьте партнером и зарабатывайте на&nbsp;своей недвижимости</p>
       </div>
-      <div class="f-column">
+      <div class="f-right">
       </div>
     </div>
   </article>
@@ -20,35 +20,50 @@ export default {
 
 <style scoped lang='sass'>
 article
-  background: #f44f0c
   color: #fff
 
   +lte_ipad
     text-align: center
     padding-top: rem(110px)
+    background-image: url(../assets/slide_01/background_upper-mobile.svg)
+    background-repeat: no-repeat
+    background-size: contain
+    min-height: rem(313px)
 
   +gte_laptop
+    /* background: #f44f0c */
     padding-top: 200px
     min-height: 600px
 
-h1
-  +lte_ipad
-    margin-bottom: 20px
-
+.fc
   +gte_laptop
-    max-width: 520px
-    margin-bottom: 24px
+    display: flex
 
-p
-  letter-spacing: -0.01em
+  .f-left
+    +gte_laptop
+      background-image: url(../assets/slide_01/background_left-desktop.svg)
+      background-repeat: no-repeat
+      background-size: contain
+      width: 63.6%
 
-  +lte_ipad
-    font-size: rem(14px)
-    line-height: rem(20px)
+    h1
+      +lte_ipad
+        margin-bottom: 20px
 
-  +gte_laptop
-    font-size: 24px
-    line-height: 28px
+      +gte_laptop
+        max-width: 520px
+        margin-bottom: 24px
+
+    p
+      letter-spacing: -0.01em
+
+      +lte_ipad
+        font-size: rem(14px)
+        line-height: rem(20px)
+
+      +gte_laptop
+        font-size: 24px
+        line-height: 28px
 
 .logo
   background-image: url(../assets/slide_01/logo.svg)
