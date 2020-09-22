e1a4c89fccd8851165c50da6f1b1bad079601496
Andrey Sidorov <takandar@gmail.com>
2020-09-22 12:38:53 +0300

    working on desktop images scaling

--- src/components/slide_02.vue
+++ src/components/slide_02.vue
@@ -22,11 +22,8 @@ export default {
 
 <style scoped lang='sass'>
 article
-  +lte_ipad
-    padding-top: rem(80px)
-
   +gte_laptop
-    padding-top: 118px
+    padding: 0
     height: 983px
 
   &:before
@@ -36,14 +33,14 @@ article
       height: scale-between(550px, 645px, 1024px, 1280px)
 
     +gte_laptop
-      background-image: url(../assets/slide_02/background-desktop.svg )
+      background-image: url(../assets/slide_02/background-desktop.svg)
       background-repeat: no-repeat
       background-size: contain
-      bottom: 0
       content: ''
-      left: 0
       position: absolute
       z-index: -1
+      top: 214px
+      left: -287px
 
     +gte_desktop
       width: 645px
@@ -53,10 +50,12 @@ h2
   text-align: center
 
   +lte_ipad
+    padding-top: rem(80px)
     margin-bottom: rem(12px)
 
   +gte_laptop
     max-width: 540px
+    padding-top: 118px
     margin: 0 auto 22px
 
 .note
@@ -78,4 +77,14 @@ h2
 img.girl
   position: absolute
   bottom: 0
+
+  +lte_ipad
+    display: none
+
+  +laptop
+    width: scale-between(390px, 516px, 1024px, 1280px)
+    left: scale-between(-85px, -185px, 1024px, 1280px)
+
+  +gte_desktop
+    left: -185px
 </style>
