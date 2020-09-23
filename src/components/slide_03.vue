d2378c5e8c454447fadfd08e533354b0954f5dcf
Andrey Sidorov <takandar@gmail.com>
2020-09-23 11:59:17 +0300

    align slide 3 content

--- src/components/slide_03.vue
+++ src/components/slide_03.vue
@@ -1,14 +1,14 @@
 <template>
-  <article>
-    <div class='container'>
+  <div class='background'>
+    <article>
       <h2>
         Кладовкин &mdash; технологический лидер российского
         рынка Self&nbsp;Storage
       </h2>
-    </div>
-    <div class='details'>
-    </div>
-  </article>
+      <div class='details'>
+      </div>
+    </article>
+  </div>
 </template>
 
 <script>
@@ -18,11 +18,14 @@ export default {
 </script>
 
 <style scoped lang='sass'>
-article
+.background
   background: #5096ff
+  overflow: hidden
+
+article
   display: flex
   flex-direction: column
-  padding: 0
+  padding-right: 0
 
   +lte_ipad
     height: rem(444px)
@@ -46,15 +49,24 @@ h2
     margin-bottom: 52px
 
 .details
-  +default_margin-left
   background: #fff
   box-shadow: 5px 4px 15px rgba(0, 0, 0, 0.05)
   border-radius: rem(40px) 0px 0px rem(40px)
   flex-grow: 1
+  position: relative
 
   +lte_ipad
     padding: rem(24px) 0 rem(24px) rem(51px)
 
   +gte_laptop
     padding: 52px 0 46px 48px
+
+  &:after
+    content: ''
+    background: #fff
+    position: absolute
+    right: -99999px
+    width: 99999px
+    height: 100%
+    top: 0
 </style>
