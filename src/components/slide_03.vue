bf0c6411e6006d5c2e9b19290cae327d65574af0
Andrey Sidorov <takandar@gmail.com>
2020-09-23 11:47:17 +0300

    working on slide 3

--- src/components/slide_03.vue
+++ src/components/slide_03.vue
@@ -1,6 +1,13 @@
 <template>
   <article>
-    Кладовкин — технологический лидер российского рынка Self Storage
+    <div class='container'>
+      <h2>
+        Кладовкин &mdash; технологический лидер российского
+        рынка Self&nbsp;Storage
+      </h2>
+    </div>
+    <div class='details'>
+    </div>
   </article>
 </template>
 
@@ -13,7 +20,41 @@ export default {
 <style scoped lang='sass'>
 article
   background: #5096ff
+  display: flex
+  flex-direction: column
+  padding: 0
+
+  +lte_ipad
+    height: rem(444px)
+    padding-bottom: rem(88px)
+    padding-top: rem(40px)
 
   +gte_laptop
     height: 660px
+    padding-bottom: 118px
+    padding-top: 122px
+
+h2
+  color: #fff
+
+  +lte_ipad
+    text-align: center
+    margin-bottom: rem(24px)
+
+  +gte_laptop
+    max-width: 585px
+    margin-bottom: 52px
+
+.details
+  +default_margin-left
+  background: #fff
+  box-shadow: 5px 4px 15px rgba(0, 0, 0, 0.05)
+  border-radius: rem(40px) 0px 0px rem(40px)
+  flex-grow: 1
+
+  +lte_ipad
+    padding: rem(24px) 0 rem(24px) rem(51px)
+
+  +gte_laptop
+    padding: 52px 0 46px 48px
 </style>
