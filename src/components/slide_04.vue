c2a323d10a480f2167468addf870306c841c8480
Andrey Sidorov <takandar@gmail.com>
2020-09-27 19:05:04 +0300

    basic switcher styles

--- src/components/slide_04.vue
+++ src/components/slide_04.vue
@@ -47,6 +47,52 @@ article
   +gte_laptop
     padding-top: 125px
 
+.tabs
+  align-items: center
+  background: #f4f4f4
+  border-radius: 40px
+  box-shadow: inset 0px 4px 10px rgba(0, 0, 0, 0.15)
+  display: flex
+  margin: 0 auto
+
+  +lte_ipad
+    height: rem(48px)
+    width: rem(288px)
+    padding: rem(4px)
+
+  +gte_laptop
+    height: 65px
+    width: 350px
+    padding: 5px
+
+  .tab
+    flex-grow: 1
+    text-align: center
+    color: #939393
+    display: flex
+    align-items: center
+    justify-content: center
+    height: 100%
+
+    +gte_laptop
+      font-size: 16px
+
+    &.is-active
+      background: #f44f0c
+      border-radius: 40px
+      box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25)
+      color: #FFF
+      font-weight: 700
+
+      +lte_ipad
+        font-size: 12px
+
+      +gte_laptop
+        font-size: 16px
+
+    &:not(.is-active)
+    cursor: pointer
+
 h2
   color: #4b4b4b
 
