0b930033f49a3aedbc83f3aacc1ab5a688eb0a2b
Andrey Sidorov <takandar@gmail.com>
2020-09-27 19:04:52 +0300

    basic slide 4

--- src/components/slide_04.vue
+++ src/components/slide_04.vue
@@ -1,14 +1,57 @@
 <template>
   <article>
-    Организуем эффективный бизнес Self Storage с помощью IT-технологий
+    <div class='tabs'>
+      <div
+        class='tab'
+        :class='{ "is-active": isOwner }'
+        @click='() => switchTab("owner")'
+      >
+        Собственникам
+      </div>
+      <div
+        class='tab'
+        :class='{ "is-active": isTenant }'
+        @click='() => switchTab("tenant")'
+      >
+        Арендаторам
+      </div>
+    </div>
+    <h2>Организуем эффективный бизнес Self Storage с помощью IT-технологий</h2>
   </article>
 </template>
 
 <script>
 export default {
-  name: 'Slide4'
+  name: 'Slide4',
+  data: () => ({
+    tab: 'owner'
+  }),
+  computed: {
+    isOwner() {
+      return this.tab === 'owner';
+    },
+    isTenant() {
+      return this.tab === 'tenant';
+    }
+  },
+  methods: {
+    switchTab(tab) {
+      this.tab = tab;
+    }
+  }
 };
 </script>
 
 <style scoped lang='sass'>
+article
+  +gte_laptop
+    padding-top: 125px
+
+h2
+  color: #4b4b4b
+
+  +gte_laptop
+    margin: 0 auto
+    max-width: 796px
+    text-align: center
 </style>
