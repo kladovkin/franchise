a89f8d9238e45fb87d3a78122e3086bace02d9d6
Andrey Sidorov <takandar@gmail.com>
2020-09-28 16:10:10 +0300

    working on slide5

--- src/components/slide_05.vue
+++ src/components/slide_05.vue
@@ -1,6 +1,23 @@
 <template>
   <article>
-    Мы работаем с юридическими лицами и ИП — собственниками помещений от 100 м²
+    <h2>
+      Мы работаем с юридическими лицами и ИП &mdash; собственниками
+      помещений от 100 м²
+    </h2>
+    <div class='details'>
+      <div class='cc-2'>
+        <div class='c-column'>
+          <div class='type-1'>Отдельно стоящее<br>складское помещение</div>
+          <div class='type-2'>Складское помещение<br>в жилом доме</div>
+          <div class='type-3'>Здания<br>бывших АТС</div>
+        </div>
+        <div class='c-column'>
+          <div class='type-4'>Подвальные<br>помещения</div>
+          <div class='type-5'>Подземный паркинг<br>торгового центра</div>
+          <div class='type-6'>Другие<br>помещения</div>
+        </div>
+      </div>
+    </div>
   </article>
 </template>
 
@@ -12,6 +29,72 @@ export default {
 
 <style scoped lang='sass'>
 article
-  background: #daf1ff
-  height: 600px
+  +gte_laptop
+    padding-top: 29px
+
+h2
+  +lte_ipad
+    margin-bottom: rem(40px)
+    text-align: center
+
+  +gte_laptop
+    margin-bottom: 51px
+    max-width: 827px
+
+.details
+  position: relative
+  background: #5096ff
+  border-radius: 0px 40px 40px 0px
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.05)
+
+  +gte_laptop
+    height: 331px
+    margin-bottom: 57px
+    padding: 60px 0
+    width: 826px
+
+    &:after
+      content: ''
+      background: #5096ff
+      position: absolute
+      left: -99999px
+      width: 99999px
+      height: 100%
+      top: 0
+
+  .cc-2
+    display: flex
+    height: 100%
+
+    .c-column
+      display: flex
+      flex-direction: column
+      height: 100%
+      justify-content: space-between
+      width: 354px
+
+      &:not(:last-child)
+        margin-right: 39px
+
+      div
+        color: #fff
+        font-size: 16px
+        font-weight: 800
+        line-height: 23px
+        padding-left: 90px
+        position: relative
+
+        &:before
+          background-repeat: no-repeat
+          background-size: contain
+          position: absolute
+          left: 0
+          top: 0
+          content: ''
+          width: 50px
+          height: 50px
+
+      @each $icon in type-1, type-2, type-3, type-4, type-5, type-6
+        .#{$icon}:before
+          background-image: url(../assets/slide_05/#{$icon}.svg)
 </style>
