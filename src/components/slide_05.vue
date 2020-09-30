53f7ac3985ef78969799fc0707b4a7fbcdffad23
Andrey Sidorov <takandar@gmail.com>
2020-09-30 10:03:06 +0300

    mobile slide 5

--- src/components/slide_05.vue
+++ src/components/slide_05.vue
@@ -1,30 +1,36 @@
 <template>
   <div class='background-cutter'>
     <article>
-      <h2>
-        Мы работаем с юридическими лицами и ИП &mdash; собственниками
-        помещений от 100 м²
-      </h2>
-      <div class='details'>
-        <div class='cc-2'>
-          <div class='c-column'>
-            <div class='type-1'>Отдельно стоящее<br>складское помещение</div>
-            <div class='type-2'>Складское помещение<br>в жилом доме</div>
-            <div class='type-3'>Здания<br>бывших АТС</div>
-          </div>
-          <div class='c-column'>
-            <div class='type-4'>Подвальные<br>помещения</div>
-            <div class='type-5'>Подземный паркинг<br>торгового центра</div>
-            <div class='type-6'>Другие<br>помещения</div>
+      <div class='container'>
+        <h2>
+          Мы работаем с юридическими лицами и ИП &mdash; собственниками
+          помещений от 100 м²
+        </h2>
+      </div>
+      <div class='background-details'>
+        <img
+          class='background'
+          loading='lazy'
+          src='../assets/slide_05/background.png'
+        />
+        <div class='details'>
+          <div class='cc-2'>
+            <div class='c-column'>
+              <div class='type-1'>Отдельно стоящее<br>складское помещение</div>
+              <div class='type-2'>Складское помещение<br>в жилом доме</div>
+              <div class='type-3'>Здания<br>бывших АТС</div>
+            </div>
+            <div class='c-column'>
+              <div class='type-4'>Подвальные<br>помещения</div>
+              <div class='type-5'>Подземный паркинг<br>торгового центра</div>
+              <div class='type-6'>Другие<br>помещения</div>
+            </div>
           </div>
         </div>
       </div>
-      <Button type='red' text='Узнать больше' @click='lead'/>
-      <img
-        class='background'
-        loading='lazy'
-        src='../assets/slide_05/background.png'
-      />
+      <div class='container'>
+        <Button type='red' text='Узнать больше' @click='lead'/>
+      </div>
     </article>
   </div>
 </template>
@@ -48,6 +54,10 @@ export default {
   overflow: hidden
 
 article
+  +lte_ipad
+    padding-left: 0
+    padding-right: 0
+
   +gte_laptop
     padding-top: 29px
     padding-bottom: 76px
@@ -73,6 +83,10 @@ article
       top: 0
       right: 0
 
+.container
+  +gte_laptop
+    padding: 0
+
 h2
   +lte_ipad
     margin-bottom: rem(40px)
@@ -87,6 +101,11 @@ h2
   background: #5096ff
   border-radius: 0px 40px 40px 0px
   box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.05)
+
+  +lte_ipad
+    margin-bottom: 24px
+    padding: rem(24px) rem(16px)
+    width: 68.4375%
 
   +gte_laptop
     height: 331px
@@ -107,31 +126,57 @@ h2
     display: flex
     height: 100%
 
+    +lte_ipad
+      flex-direction: column
+
     .c-column
       display: flex
       flex-direction: column
       height: 100%
       justify-content: space-between
-      width: 354px
+
+      +gte_laptop
+        width: 354px
 
       &:not(:last-child)
-        margin-right: 30px
+        +lte_ipad
+          margin-bottom: rem(24px)
+
+        +gte_laptop
+          margin-right: 30px
 
       div
         color: #fff
-        font-size: 16px
-        font-weight: 800
-        line-height: 23px
         display: flex
         align-items: center
+
+        +lte_ipad
+          font-weight: 500
+          font-size: rem(12px)
+          line-height: rem(16px)
+
+          &:not(:last-child)
+            margin-bottom: rem(24px)
+
+        +gte_laptop
+          font-size: 16px
+          font-weight: 800
+          line-height: 23px
 
         &:before
           background-repeat: no-repeat
           background-size: contain
           content: ''
-          width: 50px
-          height: 50px
-          margin-right: 40px
+
+          +lte_ipad
+            height: rem(35px)
+            margin-right: rem(16px)
+            width: rem(35px)
+
+          +gte_laptop
+            height: 50px
+            margin-right: 40px
+            width: 50px
 
       @each $icon in type-1, type-2, type-3, type-4, type-5, type-6
         .#{$icon}:before
@@ -141,15 +186,27 @@ h2
   +gte_laptop
     max-width: 255px
 
-.background
-  position: absolute
-  bottom: 0
-  z-index: -1
-  transform: translateX(50%)
+.background-details
+  +lte_ipad
+    position: relative
 
-  @media screen and (min-width: 1024px) and (max-width: 1200px)
-    right: scale-between(81px, 242px, 1024px, 1200px)
+  .background
+    position: absolute
+    z-index: -1
 
-  @media screen and (min-width: 1201px)
-    right: 242px
+    +lte_ipad
+      display: block
+      height: 100%
+      left: rem(-218px)
+      min-width: 100%
+
+    +gte_laptop
+      transform: translateX(50%)
+      bottom: 0
+
+    @media screen and (min-width: 1024px) and (max-width: 1200px)
+      right: scale-between(81px, 242px, 1024px, 1200px)
+
+    @media screen and (min-width: 1201px)
+      right: 242px
 </style>
