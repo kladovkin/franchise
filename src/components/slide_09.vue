c6340457e5cd43c6574cdd0b457426ff79c92d4b
Andrey Sidorov <takandar@gmail.com>
2020-10-07 11:02:54 +0300

    working on nineth slide

--- src/components/slide_09.vue
+++ src/components/slide_09.vue
@@ -1,6 +1,7 @@
 <template>
   <article>
-    Остались вопросы?
+    <h2>Остались вопросы?</h2>
+    <div class='note'>Оставьте свои контакты и мы с вами свяжемся</div>
   </article>
 </template>
 
@@ -12,6 +13,42 @@ export default {
 
 <style scoped lang='sass'>
 article
-  background: #fedddf
-  height: 600px
+  +lte_ipad
+    overflow: hidden
+    padding-top: rem(81px)
+
+  +gte_laptop
+    padding-top: 96px
+
+h2
+  +lte_ipad
+    margin-bottom: rem(12px)
+    text-align: center
+
+  +gte_laptop
+    margin-bottom: rem(16px)
+
+.note
+  color: #6c6c6c
+  letter-spacing: -0.01em
+
+  +lte_ipad
+    font-size: rem(14px)
+    line-height: rem(16px)
+    text-align: center
+    margin-bottom: rem(40px)
+
+  +gte_laptop
+    font-size: 18px
+    line-height: 23px
+    margin-bottom: 42px
+
+    &:after
+      background-image: url(../assets/slide_09/logo.svg)
+      background-repeat: no-repeat
+      background-size: contain
+      content: ''
+      height: 62px
+      position: absolute
+      width: 226px
 </style>
