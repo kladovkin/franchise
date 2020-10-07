5340ba757452e09a46a87773af02107093071105
Andrey Sidorov <takandar@gmail.com>
2020-10-07 11:32:45 +0300

    add escheck

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,17 @@ module.exports = {
     host: 'localhost',
     allowedHosts: ['localhost'],
     port: 8090
-  }
+  },
+  pluginOptions: {
+    esCheck: {
+      // [optional] uses sensible defaults if not set
+      files: [
+        // array of file patterns in glob style
+        'js/*.js'
+      ],
+
+      // [optional] default: es5
+      esVersion: 'es5'
+    }
+  },
 };
