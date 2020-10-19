b8144c49b33e5058cdb6264ca934a63adf5b400e
Andrey Sidorov <takandar@gmail.com>
2020-10-19 14:12:26 +0300

    adding eschedk

--- vue.config.js
+++ vue.config.js
@@ -13,21 +13,30 @@ module.exports = {
     host: 'localhost',
     allowedHosts: ['localhost'],
     port: 8090
-  }
+  },
+  transpileDependencies: [
+    // 'delay',
+    // 'query-string',
+    // 'strict-uri-encode',
+    // 'split-on-first',
+    // 'dom7',
+    // 'swiper',
+    // 'local-storage'
+  ],
   // uncomment to debug es-check errors
-  // chainWebpack: config => {
-  //   config.optimization.minimize(false);
-  // },
-  // pluginOptions: {
-  //   esCheck: {
-  //     // [optional] uses sensible defaults if not set
-  //     files: [
-  //       // array of file patterns in glob style
-  //       'js/*.js'
-  //     ],
-  //
-  //     // [optional] default: es5
-  //     esVersion: 'es5'
-  //   }
-  // }
+  chainWebpack: config => {
+    config.optimization.minimize(false);
+  },
+  pluginOptions: {
+    esCheck: {
+      // [optional] uses sensible defaults if not set
+      files: [
+        // array of file patterns in glob style
+        'js/*.js'
+      ],
+
+      // [optional] default: es5
+      esVersion: 'es5'
+    }
+  }
 };
