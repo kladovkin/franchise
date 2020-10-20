a77037aaf57b0f3329f9cad1b7b7b483727dec06
Andrey Sidorov <takandar@gmail.com>
2020-10-20 11:23:40 +0300

    menu close icons

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class='container'>
+  <div class='container' :class='{ "is-expanded": isExpanded }'>
     <div class='container-padding-offset'>
-      <div class='trigger' />
+      <div class='trigger'/>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Menu'
+  name: 'Menu',
+  data: () => ({
+    isExpanded: false
+  })
 };
 </script>
 
@@ -25,7 +28,7 @@ export default {
   z-index: 2
 
   +lte_ipad
-    background-image: url(../assets/menu/trigger-mobile.svg)
+    background-image: url(../assets/menu/open-mobile.svg)
     width: rem(36px)
     height: rem(36px)
     border-radius: rem(36px)
@@ -34,10 +37,10 @@ export default {
     -webkit-tap-highlight-color: transparent
 
     &:active
-      background-image: url(../assets/menu/trigger-mobile-active.svg)
+      background-image: url(../assets/menu/open-mobile-active.svg)
 
   +gte_laptop
-    background-image: url(../assets/menu/trigger-desktop.svg)
+    background-image: url(../assets/menu/open-desktop.svg)
     border-radius: 63px
     height: 63px
     width: 63px
@@ -45,5 +48,18 @@ export default {
     right: 0px
 
     &:hover
-      background-image: url(../assets/menu/trigger-desktop-hover.svg)
+      background-image: url(../assets/menu/open-desktop-hover.svg)
+
+  .is-expanded &
+    +lte_ipad
+      background-image: url(../assets/menu/close-mobile.svg)
+
+      &:active
+        background-image: url(../assets/menu/close-mobile-active.svg)
+
+    +gte_laptop
+      background-image: url(../assets/menu/close-desktop.svg)
+
+      &:hover
+        background-image: url(../assets/menu/close-desktop-hover.svg)
 </style>
