55c0e848f10fde6b322686a3297cdced2ef0ab1e
Andrey Sidorov <takandar@gmail.com>
2020-10-20 11:15:47 +0300

    desktop and mobile hover/active

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -20,6 +20,7 @@ export default {
   background-repeat: no-repeat
   background-size: contain
   box-shadow: 0px 8px 16px rgba(244, 79, 12, 0.24)
+  cursor: pointer
   position: absolute
   z-index: 2
 
@@ -30,6 +31,10 @@ export default {
     border-radius: rem(36px)
     top: rem(21px)
     right: 0
+    -webkit-tap-highlight-color: transparent
+
+    &:active
+      background-image: url(../assets/menu/trigger-mobile-active.svg)
 
   +gte_laptop
     background-image: url(../assets/menu/trigger-desktop.svg)
@@ -38,4 +43,7 @@ export default {
     width: 63px
     top: 37px
     right: 0px
+
+    &:hover
+      background-image: url(../assets/menu/trigger-desktop-hover.svg)
 </style>
