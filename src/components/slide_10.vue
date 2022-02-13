70cce09c5d4bf4111e0c555e4965eb6ac34e92fa
Andrey Sidorov <takandar@gmail.com>
2022-02-13 10:31:57 +0300

    temporarily disable swiper

--- src/components/slide_10.vue
+++ src/components/slide_10.vue
@@ -2,14 +2,14 @@
   <article id='slide_10'>
     <h2>{{ t('slide_10.h2') }}</h2>
 
-    <Swiper
-      :slides-per-view='4'
-      :space-between='50'
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-    </swiper>
+    <!-- <Swiper                              -->
+    <!--   :slides-per-view='4'               -->
+    <!--   :space-between='50'                -->
+    <!-- >                                    -->
+    <!--   <SwiperSlide>Slide 1</SwiperSlide> -->
+    <!--   <SwiperSlide>Slide 2</SwiperSlide> -->
+    <!--   <SwiperSlide>Slide 3</SwiperSlide> -->
+    <!-- </swiper>                            -->
     <!-- <div ref='swiper' class='swiper'> -->
     <!--   <swiper-slide>                  -->
     <!--   <div class='slides'>            -->
@@ -24,15 +24,15 @@
 
 <script>
 import t from '@/utils/locale';
-import { Swiper, SwiperSlide } from 'swiper/vue';
+// import { Swiper, SwiperSlide } from 'swiper/vue';
 // import isMobile from '@/utils/is_mobile';
 
 export default {
   name: 'Slide8',
-  components: {
-    Swiper,
-    SwiperSlide
-  },
+  // components: {
+  //   Swiper,
+  //   SwiperSlide
+  // },
   // async mounted() {
   //   const { default: Swiper } = await import('swiper');
   //
