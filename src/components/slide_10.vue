d1d68e1e99c6ac1545607a0bc2f5a9811a23fb02
Andrey Sidorov <takandar@gmail.com>
2022-02-13 11:27:10 +0300

    partially configured swiper

--- src/components/slide_10.vue
+++ src/components/slide_10.vue
@@ -3,24 +3,15 @@
     <h2>{{ t('slide_10.h2') }}</h2>
 
     <div ref='swiper' class='swiper'>
-      <div class='slides'>
-        <div
+      <div class='swiper-wrapper'>
+        <img
           v-for='imageIndex in images'
           :key='imageIndex'
+          class='slide'
+          :src='`/slide_10/slide_image-${imageIndex}.png`'
+          :srcset='`/slide_10/slide_image-${imageIndex}@2x.png 2x`'
+          @click='enlarge(imageIndex)'
         >
-          <picture>
-            <source
-              :srcset='`/slide_10/slide_image-${blockIndex}.webp, /slide_10/slide_image-${blockIndex}@2x.webp 2x`'
-              type='image/webp'
-            >
-            <img
-              class='laptop'
-              :src='`/slide_08/slide_image-${blockIndex}.png`'
-              :srcset='`/slide_08/slide_image-${blockIndex}@2x.png 2x`'
-              @click='enlarge(blockIndex)'
-            >
-          </picture>
-
       </div>
       <div class='navigation'>
         <button class='prev' />
@@ -28,11 +19,26 @@
       </div>
     </div>
   </article>
+        <!-- <picture                                                                                                 -->
+        <!--   v-for='imageIndex in images'                                                                           -->
+        <!--   :key='imageIndex'                                                                                      -->
+        <!--   class='slide'                                                                                          -->
+        <!-- >                                                                                                        -->
+        <!--   <source                                                                                                -->
+        <!--     :srcset='`/slide_10/slide_image-${imageIndex}.webp, /slide_10/slide_image-${imageIndex}@2x.webp 2x`' -->
+        <!--     type='image/webp'                                                                                    -->
+        <!--   >                                                                                                      -->
+        <!--   <img                                                                                                   -->
+        <!--     class='laptop'                                                                                       -->
+        <!--     :src='`/slide_10/slide_image-${imageIndex}.png`'                                                     -->
+        <!--     :srcset='`/slide_10/slide_image-${imageIndex}@2x.png 2x`'                                            -->
+        <!--     @click='enlarge(imageIndex)'                                                                         -->
+        <!--   >                                                                                                      -->
+        <!-- </picture>                                                                                               -->
 </template>
 
 <script>
 import t from '@/utils/locale';
-import isMobile from '@/utils/is_mobile';
 
 export default {
   name: 'Slide8',
@@ -41,16 +47,36 @@ export default {
   //   SwiperSlidp
   // },
   data: () => ({
-    images: [0, 1, 2, 3]
+    images: [0, 1, 2, 3, 4, 5]
   }),
   async mounted() {
     const { Swiper, Navigation } = await import('swiper');
 
+    // console.log(isMobile() ? 'auto' : 4);
     this.swiper = new Swiper(this.$refs.swiper, {
-      wrapperClass: 'slides',
+      wrapperClass: 'swiper-wrapper',
       slideClass: 'slide',
-      slidesPerView: isMobile() ? 'auto' : 4,
+      loop: false,
+
+      slidesPerView: 'auto',
       spaceBetween: 0,
+      initialSlide: 0,
+
+      breakpoints: {
+        320: {
+          slidesPerView: 'auto',
+          spaceBetween: 16
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 23
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 28
+        }
+      },
+
       navigation: {
         nextEl: '.next',
         prevEl: '.prev'
@@ -78,10 +104,39 @@ article
 h2
   text-align: center
 
-.slide
-  border-radius: rem(12px)
+  +lte_ipad
+    margin-bottom: rem(24px)
 
+  +laptop
+    margin-bottom: scale-laptop(36px, 58px)
+
+  +gte_desktop
+    margin-bottom: 58px
+
+
+.swiper
+  list-style: none
+  margin-left: auto
+  margin-right: auto
+  overflow: hidden
+  padding: 0
+  position: relative
+  width: 100%
+  z-index: 1
+
+  &-pointer-events
+    touch-action: pan-y
+
+  .swiper-wrapper
+    box-sizing: content-box
+    display: flex
+    position: relative
+    transform: translate3d(0px, 0, 0)
+    transition-property: transform
+    z-index: 1
+
+  .slide
+    border-radius: rem(12px)
 // cp public/slide_10/original/* public/slide_10/; for image in public/slide_10/*@2x.png; do; echo $image; convert -resize 50% $image ${image/@2x/}; convert -quality 95% $image ${image/\.png/.webp}; convert -resize 50% ${image/\.png/.webp} ${${image/\.png/.webp}/@2x/}; tinypng $image; tinypng ${image/@2x/}; done;
-picture
-  align-self: center
+/* picture */
 </style>
