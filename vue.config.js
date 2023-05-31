f81e1d20168858b53a5b50e3749e4476ffd072b8
Andrey Sidorov <takandar@gmail.com>
2023-05-31 14:24:21 +0300

    remove es-check

--- vue.config.js
+++ vue.config.js
@@ -25,17 +25,5 @@ module.exports = {
   // chainWebpack: config => {
   //   config.optimization.minimize(false);
   // },
-  pluginOptions: {
-    esCheck: {
-      // [optional] uses sensible defaults if not set
-      files: [
-        // array of file patterns in glob style
-        'js/*.js'
-      ],
-
-      // [optional] default: es5
-      esVersion: 'es5'
-    }
-  },
   productionSourceMap: false
 };
